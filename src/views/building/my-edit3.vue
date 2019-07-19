b559ce90978f9d252a6048d57db35c5cf79056c1
huanggengzhong <915835528@qq.com>
2019-07-19 11:41:21 +0800

    完成了项目3

--- src/views/building/my-edit3.vue
+++ src/views/building/my-edit3.vue
@@ -15,9 +15,8 @@
                 </el-select>
             </el-form-item>
 
-            <el-form-item label="添加相册图片:">
+            <el-form-item label="添加相册图片:" v-show="isShow2">
 
-             
                 <el-upload action="https://salesv2-ccw-test.colourlife.com/upload/image/5" list-type="picture-card" :auto-upload="true" name="photo" :on-success="mySuccess">
                     <i slot="default" class="el-icon-plus"></i>
                     <div slot="file" slot-scope="{file}">
@@ -45,8 +44,8 @@
             </el-form-item> -->
             <div v-show="isShow">
                 <el-form-item label="添加户型分类:">
-                    <el-input v-model="form.name" class="my-input"></el-input>&nbsp;
-                    <el-button type="primary">添加</el-button>
+                    <el-input v-model="form.hxValue" class="my-input"></el-input>&nbsp;
+                    <el-button type="primary" @click="addHuxing">添加</el-button>
                 </el-form-item>
                 <el-form-item label="选择户型类型:">
                     <!-- huxingValue -->
@@ -81,29 +80,30 @@
                         <el-radio :label="3">已售罄</el-radio>
                     </el-radio-group>
                 </el-form-item> -->
-                    <el-radio-group v-model="form.status">
+<el-radio-group v-model="form.status" @change="danxuanChange">
+
                         <el-radio :label="1">是</el-radio>
                         <el-radio :label="2">否</el-radio>
 
                     </el-radio-group>
 
                     <br>
-                    <div>
+                    <div v-show="biaoqianShow">
                         <el-tag :key="tag" v-for="tag in dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
                             {{tag}}
                         </el-tag>
-                        <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+                        <el-input class="input-new-tag" v-if="inputVisible" v-model="tagtValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
                         </el-input>
-                        <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+                        <el-button v-else class="button-new-tag" size="small" @click="showInput">+新标签</el-button>
                     </div>
 
                 </el-form-item>
-                <el-form-item label="原生form测试:">
-                    <form action="https://salesv2-ccw-test.colourlife.com/upload/image/5" method="post" enctype="multipart/form-data">
-                        <input type="file" name="photo">
-                        <button>提交</button>
-                    </form>
-                </el-form-item>
+                <!-- <el-form-item label="原生form测试:">
+          <form action="https://salesv2-ccw-test.colourlife.com/upload/image/5" method="post" enctype="multipart/form-data">
+            <input type="file" name="photo">
+            <button>提交</button>
+          </form>
+        </el-form-item> -->
                 <el-form-item label="添加户型图片:">
                     <el-upload action="https://salesv2-ccw-test.colourlife.com/upload/image/5" list-type="picture-card" :auto-upload="true" name="photo" :on-success="mySuccess">
                         <i slot="default" class="el-icon-plus"></i>
@@ -123,7 +123,6 @@
                         </div>
                     </el-upload>
                 </el-form-item>
-
                 <el-form-item label="添加效果图图片:">
                     <el-upload action="https://salesv2-ccw-test.colourlife.com/upload/image/5" list-type="picture-card" :auto-upload="true" name="photo" :on-success="mySuccess">
                         <i slot="default" class="el-icon-plus"></i>
@@ -160,23 +159,51 @@ import httpClient from '@/utils/request'
 export default {
     data() {
         return {
+ biaoqianShow:true,
+
+            imagesValue: [],
+            tagtValue: '',
+
+            bid: '',
             inputValue: '', //客户选择的值
+            isShow2: true,
             isShow: false,
-            categeryValue: [
-                { name: '效果图' },
-                { name: '沙盘' },
-                { name: '户型' },
-                { name: '周边' },
-            ], //相册分类
+            // categeryValue: [{
+            //     name: '效果图'
+            //   },
+            //   {
+            //     name: '沙盘'
+            //   },
+            //   {
+            //     name: '户型'
+            //   },
+            //   {
+            //     name: '周边'
+            //   },
+            //
+            categeryValue: [],
+
+            //相册分类
             huxingValue: [
-                { name: '三室一厅' },
-                { name: '三室两厅' },
-                { name: '二室两厅' },
-                { name: '二室两厅' },
+                {
+                    name: '三室一厅',
+                },
+                {
+                    name: '三室两厅',
+                },
+                {
+                    name: '二室两厅',
+                },
+                {
+                    name: '二室两厅',
+                },
             ], //相册分类
 
             form: {
                 name: '',
+                hxValue: '',
+status:'',
+
                 money: null,
                 categery: '',
                 area1: null,
@@ -193,7 +220,7 @@ export default {
             },
             dialogImageUrl: '',
             dialogVisible: false,
-            dynamicTags: ['标签一', '标签二', '标签三'],
+            dynamicTags: ['公交直达'],
             inputVisible: false,
             inputValue: '',
             // 最后一个上传图片
@@ -205,39 +232,92 @@ export default {
     mounted() {
         // 可以得到id
         // console.log(this.$route.params.id)
-        // 获取相册分类
-        httpClient
-            .get('admin/album/category', {})
-            .then(res => {
-                console.log(res)
-                console.log(res.content)
-                let arr1 = []
-                arr1 = res.content.filter(v => {
-                    if (v.pid != 0) {
-                        return true
-                    }
-                })
-                let arr2 = res.content.filter(v => {
-                    if (v.pid == 0) {
-                        return true
-                    }
-                })
-                this.huxingValue = arr1
+        this.bid = this.$route.params.id
 
-                this.categeryValue = arr2
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        this.getMount()
     },
     methods: {
+ danxuanChange(){
+     console.log("单选变化了");
+ console.log(this.form.status);
+ if(this.form.status==2){
+this.biaoqianShow=false;
+//标签内容去空
+this.dynamicTags=[]
+
+
+ }else{
+this.biaoqianShow=true;
+this.dynamicTags=['公交直达']
+
+ }
+
+     
+     
+ },
+
+        getMount() {
+            // 获取相册分类
+            httpClient
+                .get('admin/album/category')
+                .then(res => {
+                    // console.log(res)
+                    // console.log(res.content)
+                    let arr1 = []
+                    arr1 = res.content.list.filter(v => {
+                        if (v.pid != 0) {
+                            return true
+                        }
+                    })
+                    let arr2 = res.content.list.filter(v => {
+                        if (v.pid == 0) {
+                            return true
+                        }
+                    })
+                    console.log(arr2)
+
+                    this.huxingValue = arr1
+                    this.categeryValue = arr2
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        //添加户型
+        addHuxing() {
+            //  admin/add/album/category
+            httpClient
+                .post('admin/add/album/category', {
+                    pid: 3,
+                    name: this.form.hxValue,
+                })
+                .then(res => {
+                    console.log(res)
+                    //重新调用获取
+                    this.getMount()
+                    if (res.code === 0) {
+                        this.$message({ message: res.message, type: 'success' })
+                    } else {
+                        this.$message({
+                            message: res.message,
+                            type: 'warning',
+                        })
+                    }
+                    this.form.hxValue = ''
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
         onChangeValue(e) {
             console.log(this.inputValue)
             if (this.inputValue == '户型') {
                 console.log('显示')
                 this.isShow = true
+                this.isShow2 = false
             } else {
                 this.isShow = false
+                this.isShow2 = true
             }
         },
 
@@ -247,6 +327,39 @@ export default {
 
         onSubmit() {
             console.log('上传')
+            // 获得id
+            let cidArr = this.categeryValue.filter(v => {
+                if (v.name == this.inputValue) {
+                    return true
+                }
+            })
+            let cid = cidArr[0].id
+            console.log(cid)
+            //保存请求
+            httpClient
+                .post('admin/add/image', {
+                    bid: this.bid,
+                    cid: cid,
+                    // 数据记得要写活
+                    images: this.imagesValue,
+
+                    area: this.form.area1,
+                    tags: this.dynamicTags,
+                })
+                .then(res => {
+                    console.log(res)
+                    if (res.code === 0) {
+                        this.$message({ message: res.message, type: 'success' })
+                    } else {
+                        this.$message({
+                            message: res.message,
+                            type: 'warning',
+                        })
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         },
 
         handleClose(tag) {
@@ -259,12 +372,12 @@ export default {
             })
         },
         handleInputConfirm() {
-            let inputValue = this.inputValue
-            if (inputValue) {
-                this.dynamicTags.push(inputValue)
+            let tagtValue = this.tagtValue
+            if (tagtValue) {
+                this.dynamicTags.push(tagtValue)
             }
             this.inputVisible = false
-            this.inputValue = ''
+            this.tagtValue = ''
         },
         addCategary() {
             httpClient
@@ -273,6 +386,17 @@ export default {
                 })
                 .then(res => {
                     console.log(res)
+                    this.getMount()
+
+                    if (res.code === 0) {
+                        this.$message({ message: res.message, type: 'success' })
+                    } else {
+                        this.$message({
+                            message: res.message,
+                            type: 'warning',
+                        })
+                    }
+                    this.form.inputValue = ''
                 })
                 .catch(err => {
                     console.log(err)
@@ -290,6 +414,7 @@ export default {
             console.log('进到这里,已上传')
 
             console.log(res)
+            this.imagesValue.push(res.content.photoPath)
         },
     },
 }
@@ -316,9 +441,11 @@ export default {
     top: 0;
     left: 450px;
 }
+
 .el-tag + .el-tag {
     margin-left: 10px;
 }
+
 .button-new-tag {
     margin-left: 10px;
     height: 32px;
@@ -326,11 +453,10 @@ export default {
     padding-top: 0;
     padding-bottom: 0;
 }
+
 .input-new-tag {
     width: 90px;
     margin-left: 10px;
     vertical-align: bottom;
 }
 </style>
-
-
